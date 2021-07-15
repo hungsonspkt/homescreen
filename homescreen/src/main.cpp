44b0031e7b33b4e8d7d7a6c741a83424d2439028
DESKTOP-5J11CSC\SonLe <hungsonspkt@gmail.com>
2021-07-15 21:07:42 +0700

    serial 6

--- homescreen/src/main.cpp
+++ homescreen/src/main.cpp
@@ -220,6 +220,41 @@ int main(int argc, char *argv[])
     bool is_demo_val = false;
 
     int USB = open( "/dev/ttyS0", O_RDWR| O_NOCTTY );
+    struct termios tty;
+	struct termios tty_old;
+	memset (&tty, 0, sizeof tty);
+
+	/* Error Handling */
+	if ( tcgetattr ( USB, &tty ) != 0 ) {
+	   std::cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << std::endl;
+	}
+
+	/* Save old tty parameters */
+	tty_old = tty;
+
+	/* Set Baud Rate */
+	cfsetospeed (&tty, (speed_t)B115200);
+	cfsetispeed (&tty, (speed_t)B115200);
+
+	/* Setting other Port Stuff */
+	tty.c_cflag     &=  ~PARENB;            // Make 8n1
+	tty.c_cflag     &=  ~CSTOPB;
+	tty.c_cflag     &=  ~CSIZE;
+	tty.c_cflag     |=  CS8;
+
+	tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+	tty.c_cc[VMIN]   =  1;                  // read doesn't block
+	tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+	tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+	/* Make raw */
+	cfmakeraw(&tty);
+
+	/* Flush Port, then applies attributes */
+	tcflush( USB, TCIFLUSH );
+	if ( tcsetattr ( USB, TCSANOW, &tty ) != 0) {
+	   std::cout << "Error " << errno << " from tcsetattr" << std::endl;
+	}
 
     QPlatformNativeInterface *native = qApp->platformNativeInterface();
     struct agl_shell *agl_shell = nullptr;
