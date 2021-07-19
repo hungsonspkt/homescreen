2b31a9ac4a221901b9b0d4901a0b9816354d8087
DESKTOP-5J11CSC\SonLe <hungsonspkt@gmail.com>
2021-07-19 18:36:50 +0700

    serial 9

--- homescreen/src/main.cpp
+++ homescreen/src/main.cpp
@@ -57,12 +57,50 @@ int fdUSB = 0x00;
 
 void* doSomeThing(void *arg)
 {
+	usleep(30000000);//30s
+	fdUSB = open( "/dev/ttyS0", O_RDWR| O_NOCTTY );
+    struct termios tty;
+	struct termios tty_old;
+	memset (&tty, 0, sizeof tty);
+
+	/* Error Handling */
+	if ( tcgetattr ( fdUSB, &tty ) != 0 ) {
+	   printf("error tcgetattr\n");
+	}
+
+	/* Save old tty parameters */
+	tty_old = tty;
+
+	/* Set Baud Rate */
+	cfsetospeed (&tty, (speed_t)B115200);
+	cfsetispeed (&tty, (speed_t)B115200);
+
+	/* Setting other Port Stuff */
+	tty.c_cflag     &=  ~PARENB;            // Make 8n1
+	tty.c_cflag     &=  ~CSTOPB;
+	tty.c_cflag     &=  ~CSIZE;
+	tty.c_cflag     |=  CS8;
+
+	tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+	tty.c_cc[VMIN]   =  1;                  // read doesn't block
+	tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+	tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+	/* Make raw */
+	cfmakeraw(&tty);
+
+	/* Flush Port, then applies attributes */
+	tcflush( fdUSB, TCIFLUSH );
+	if ( tcsetattr ( fdUSB, TCSANOW, &tty ) != 0) {
+	   printf("flush serial bufer failed\n");
+	}
+	printk("[HS] start thread");
     while(1)
     {
     	if(fdUSB != 0x00)
     	{
     		write (fdUSB, "K-Auto hello!\n", strlen("K-Auto hello!\n")); 
-    		usleep(1000000);//1s
+    		usleep(10000);//1s
     	}
     }
 
@@ -244,43 +282,6 @@ int main(int argc, char *argv[])
     QGuiApplication a(argc, argv);
     const char *screen_name;
     bool is_demo_val = false;
-
-    fdUSB = open( "/dev/ttyS0", O_RDWR| O_NOCTTY );
-    struct termios tty;
-	struct termios tty_old;
-	memset (&tty, 0, sizeof tty);
-
-	/* Error Handling */
-	if ( tcgetattr ( fdUSB, &tty ) != 0 ) {
-	   printf("error tcgetattr\n");
-	}
-
-	/* Save old tty parameters */
-	tty_old = tty;
-
-	/* Set Baud Rate */
-	cfsetospeed (&tty, (speed_t)B115200);
-	cfsetispeed (&tty, (speed_t)B115200);
-
-	/* Setting other Port Stuff */
-	tty.c_cflag     &=  ~PARENB;            // Make 8n1
-	tty.c_cflag     &=  ~CSTOPB;
-	tty.c_cflag     &=  ~CSIZE;
-	tty.c_cflag     |=  CS8;
-
-	tty.c_cflag     &=  ~CRTSCTS;           // no flow control
-	tty.c_cc[VMIN]   =  1;                  // read doesn't block
-	tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
-	tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
-
-	/* Make raw */
-	cfmakeraw(&tty);
-
-	/* Flush Port, then applies attributes */
-	tcflush( fdUSB, TCIFLUSH );
-	if ( tcsetattr ( fdUSB, TCSANOW, &tty ) != 0) {
-	   printf("flush serial bufer failed\n");
-	}
 
 
 	pthread_create(&tid, NULL, &doSomeThing, NULL);
